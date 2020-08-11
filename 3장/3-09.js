a85f5415b6c45da963317aba78a64a9dbf6865d7
Heo Dong <gjehd3@naver.com>
2020-08-11 21:59:56 +0900

    예제 3-9 코드 수정본

--- 3장/3-09.js
+++ 3장/3-09.js
@@ -7,9 +7,9 @@ var obj1 = {
       innerFunc();
   
       var obj2 = {
-        innerMethod: innerFunc,
+        innerMethod: innerFunc
       };
       obj2.innerMethod();
-    },
+    }
   };
   obj1.outer();
