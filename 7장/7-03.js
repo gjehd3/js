ba526ff916cabef8fb5ec6edcdca1b3247887df3
Heo Dong <gjehd3@naver.com>
2020-08-21 17:02:50 +0900

    예제 7-3 : lenght 프로퍼티를 삭제한 경우(수정본)

--- 7장/7-03.js
+++ 7장/7-03.js
@@ -11,4 +11,4 @@ var Grade = function() {
   console.log(g); // Grade { 0: 100, 1: 80, 2: 90, length: 3 }
   delete g.length
   g.push(70);
-  console.log(g); // Grade { 0: 70, 1: 80, 2: 90, length: 3 }
+  console.log(g); // Grade { 0: 70, 1: 80, 2: 90, length: 1 }
